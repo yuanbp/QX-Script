a185282663156e4cfef36ac9aeb637674bca6fff
yuanbp <yuanbp0912@163.com>
2022-10-03 23:20:33 +0800

    Update 12306.js

--- 12306.js
+++ 12306.js
@@ -42,7 +42,10 @@ var body = JSON.parse(res);
 body['code'] = '00';
 body['materialsList'] = [];
 body['rid'] = '';
-//body['advertParam']['marginBottom'] = 0;
+body['advertParam'] = {};
+
+/**
+body['advertParam']['marginBottom'] = 0;
 body['advertParam']['skipTime'] = 0;
 body['advertParam']['showSkipBtn'] = 0;
 body['advertParam']['skipTimeAgain'] = 0;
@@ -52,6 +55,7 @@ body['advertParam']['isDefault'] = 1;
 body['advertParam']['displayNumDi'] = 0;
 body['advertParam']['index'] = 0;
 body['advertParam']['bs'] = 0;
+*/
 
 result = JSON.stringify(body);
 
