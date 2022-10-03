729f27629f106163ed856481097597bd5a630acb
yuanbp <yuanbp0912@163.com>
2022-10-03 22:28:03 +0800

    Update 12306.js

--- 12306.js
+++ 12306.js
@@ -1,7 +1,47 @@
+/** {
+  "code": "00",
+  "materialsList": [{
+    "billId": "4501",
+    "billMaterialsId": "6085",
+    "filePath": "https://ad.12306.cn/res/adfiles//delivery/2022/09/30/c72732f894ec471194b4c8bcb7c3dc45.jpg",
+    "title": "12306约车",
+    "linkUri": "app#60000020#/www/index.html?showTitleBar=false&channel=003",
+    "linkType": 3,
+    "viewUrlList": ["https://ad.12306.cn/ad/mon/mzc?bid=4501&appId=2&mid=6085&pn=0007&n=1&ct=0&cn=CH&reqDate=1664784537511&rid=e01bdd1956fd491db99dec7d55979911&did=3AC59762-7C09-43DC-89A6-83410D0F774F&t=1"],
+    "clickUrlList": ["https://ad.12306.cn/ad/mon/mzc?bid=4501&appId=2&mid=6085&pn=0007&n=1&ct=0&cn=CH&reqDate=1664784537511&rid=e01bdd1956fd491db99dec7d55979911&did=3AC59762-7C09-43DC-89A6-83410D0F774F&t=2&w=__WIDTH__&h=__HEIGHT__&dx=__DOWN_X__&dy=__DOWN_Y__&ux=__UP_X__&uy=__UP_Y__"],
+    "textDesc": "",
+    "dplUrl": "",
+    "advNature": 4,
+    "pn": "0007",
+    "creativeType": 1,
+    "dc": 1,
+    "dd": "向上滑动#跳转至详情页",
+    "dv": 0,
+    "ds": 6,
+    "mp": 30
+  }],
+  "rid": "e01bdd1956fd491db99dec7d55979911",
+  "advertParam": {
+    "marginBottom": 15,
+    "skipTime": 3000,
+    "showSkipBtn": 1,
+    "skipTimeAgain": 20,
+    "chacheTime": 600000,
+    "fixedscreen": 3,
+    "isFullScreen": 0,
+    "isDefault": 0,
+    "displayNumDi": 1,
+    "index": 0,
+    "bs": 2
+  }
+} */
+
 var res = $response.body;
 var body = JSON.parse(res);
 
 body['code'] = 99;
+body['materialsList'] = [];
+body['advertParam']['showSkipBtn'] = 0;
 result = JSON.stringify(body);
 
 console.log(result);
