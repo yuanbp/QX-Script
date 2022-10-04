47d61c6eac9b5f6ec350287326a766047e877c6f
yuanbp <yuanbp0912@163.com>
2022-10-04 14:15:00 +0800

    Create bilibiliTab.js

---
+++ bilibiliTab.js
@@ -0,0 +1,17 @@
+let whitelist=['追番','推荐','动画','影视']
+
+let body = $response.body
+body=JSON.parse(body)
+
+body['data']['tab'].forEach((element, index) => {
+if(!(whitelist.includes(element['name']))) body['data']['tab'].splice(index,1)  
+});
+
+body['data']['bottom'].forEach((element, index)=> {
+    if(element['pos']==4){      
+       body['data']['bottom'].splice(index,1)  
+    }
+})
+
+body=JSON.stringify(body)
+$done({body}) 
