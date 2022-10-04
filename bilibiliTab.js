ce9b37d06962a125fc2a621960958624d06f9271
yuanbp <yuanbp0912@163.com>
2022-10-04 15:06:33 +0800

    Update bilibiliTab.js

--- bilibiliTab.js
+++ bilibiliTab.js
@@ -4,11 +4,7 @@ let body = $response.body
 body=JSON.parse(body)
 
 body['data']['tab'].forEach((element, index) => {
-  console.log(element['name']);
-  if(!(whitelist.includes(element['name']))) {
-    body['data']['tab'].splice(index,1);
-    console.log(element['name']);
-  }  
+  console.log(element['name']);  
 });
 
 body['data']['bottom'].forEach((element, index)=> {
