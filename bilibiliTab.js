4f454c35faf19c26c90848facb450d40acd20c3b
yuanbp <yuanbp0912@163.com>
2022-10-04 15:16:48 +0800

    Update bilibiliTab.js

--- bilibiliTab.js
+++ bilibiliTab.js
@@ -3,15 +3,10 @@ let whitelist=['追番','推荐','动画','影视']
 let body = $response.body
 body=JSON.parse(body)
 
-body['data']['tab'].forEach((element, index) => {
-  console.log(element['name']);  
+let tabArr = body['data']['tab'].filter((element) => {
+  return whitelist.includes(element['name'])(
 });
-
-body['data']['bottom'].forEach((element, index)=> {
-    if(element['pos']==4){      
-       body['data']['bottom'].splice(index,1)  
-    }
-})
+body['data']['tab'] = tabArr;
 
 body=JSON.stringify(body)
 $done({body})
