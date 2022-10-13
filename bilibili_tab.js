eb710df24913cd704371ffccd52a92e67d51da0c
yuanbp <yuanbp0912@163.com>
2022-10-13 11:32:53 +0800

    Update bilibili_tab.js

--- bilibili_tab.js
+++ bilibili_tab.js
@@ -4,18 +4,22 @@ let bottomlist=['首页','动态','我的']
 let body = $response.body;
 body=JSON.parse(body);
 
-let tabArr=body['data']['tab'].filter((element) => {
+//let tabArr=body['data']['tab'].filter((element) => {
+let tabArr=body.data.tab.filter((element) => {
   //console.log(element);
-  return tablist.includes(element['name']);
+  //return tablist.includes(element['name']);
+    return tablist.includes(element.name);
 });
 //body['data']['tab'].splice(0,body['data']['tab'].length)
 //body['data']['tab'].push(...tabArr);
 body.data.tab=tabArr;
 //console.log(body['data']['tab']);
 
-let bottomArr=body['data']['bottom'].filter((element) => {
+//let bottomArr=body['data']['bottom'].filter((element) => {
+let bottomArr=body.data.bottom.filter((element) => {
   //console.log(element.name);
-  return bottomlist.includes(element['name']);
+  //return bottomlist.includes(element['name']);
+    return bottomlist.includes(element.name);
 });
 //console.log(bottomArr.length);
 //body['data']['bottom'].splice(0,body['data']['bottom'].length)
