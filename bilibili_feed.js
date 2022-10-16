b88a66067eeb2794f767954ad72dbba99f69b6d9
yuanbp <yuanbp0912@163.com>
2022-10-16 16:13:00 +0800

    Update bilibili_feed.js

--- bilibili_feed.js
+++ bilibili_feed.js
@@ -3,7 +3,7 @@ let body = JSON.parse(res);
 
 let items=body['data']['items'].filter(e => {
   if(e.hasOwnProperty('banner_item')){
-    e['banner_item']=[];
+    delete e['banner_item'];
   }
   return ((e.hasOwnProperty('card_goto') && !e['card_goto'].startsWith('ad_')) && !e.hasOwnProperty('ad_info'));
 })
