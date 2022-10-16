bd3ffd891e71d88a1ddf3915fc7b943091b5e66a
yuanbp <yuanbp0912@163.com>
2022-10-16 15:54:20 +0800

    Create bilibili_feed.js

---
+++ bilibili_feed.js
@@ -0,0 +1,16 @@
+let res = $response.body;
+let body = JSON.parse(res);
+
+let items=body['data']['items'].filter(e -> {
+  if(e.hasOwnProperty('banner_item')){
+    e['banner_item']=[];
+  }
+  return ((e.hasOwnProperty('card_goto') && !e['card_goto'].startsWith('ad_')) && !e.hasOwnProperty('ad_info'));
+})
+body['data']['items']=items;
+
+let result = JSON.stringify(body);
+
+//console.log(result);
+
+$done(result);
