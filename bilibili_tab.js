b0b5d749de30141ba7e4a27715dcf5b11bcb12c4
yuanbp <yuanbp0912@163.com>
2022-10-18 10:58:47 +0800

    Update bilibili_tab.js

--- bilibili_tab.js
+++ bilibili_tab.js
@@ -1,36 +1,36 @@
 try{
-let tablist=['追番','推荐','动画','影视'];
-let bottomlist=['首页','动态','我的']
+  let tablist=['追番','推荐','动画','影视'];
+  let bottomlist=['首页','动态','我的']
 
-let body = $response.body;
-body=JSON.parse(body);
+  let body = $response.body;
+  body=JSON.parse(body);
 
-let tabArr=body['data']['tab'].filter((element) => {
-//let tabArr=body.data.tab.filter((element) => {
-  //console.log(element);
-  return tablist.includes(element['name']);
-  //return tablist.includes(element.name);
+  let tabArr=body['data']['tab'].filter((element) => {
+  //let tabArr=body.data.tab.filter((element) => {
+    //console.log(element);
+    return tablist.includes(element['name']);
+    //return tablist.includes(element.name);
 });
-//body['data']['tab'].splice(0,body['data']['tab'].length)
-//body['data']['tab'].push(...tabArr);
-//body.data.tab=tabArr;
-body['data']['tab']=tabArr;
-//console.log(body['data']['tab']);
+  //body['data']['tab'].splice(0,body['data']['tab'].length)
+  //body['data']['tab'].push(...tabArr);
+  //body.data.tab=tabArr;
+  body['data']['tab']=tabArr;
+  //console.log(body['data']['tab']);
 
-let bottomArr=body['data']['bottom'].filter((element) => {
-//let bottomArr=body.data.bottom.filter((element) => {
-  //console.log(element.name);
-  return bottomlist.includes(element['name']);
-  //return bottomlist.includes(element.name);
+  let bottomArr=body['data']['bottom'].filter((element) => {
+  //let bottomArr=body.data.bottom.filter((element) => {
+    //console.log(element.name);
+    return bottomlist.includes(element['name']);
+    //return bottomlist.includes(element.name);
 });
-//console.log(bottomArr.length);
-//body['data']['bottom'].splice(0,body['data']['bottom'].length)
-//body['data']['bottom'].push(...bottomArr);
-//body.data.bottom=bottomArr;
-body['data']['bottom']=bottomArr;
+  //console.log(bottomArr.length);
+  //body['data']['bottom'].splice(0,body['data']['bottom'].length)
+  //body['data']['bottom'].push(...bottomArr);
+  //body.data.bottom=bottomArr;
+  body['data']['bottom']=bottomArr;
 
-body=JSON.stringify(body);
-$done({body});
+  body=JSON.stringify(body);
+  $done({body});
 } catch (err) {
-          magicJS.logError(`直播去广告出现异常：${err}`);
-        }
+  magicJS.logError(`直播去广告出现异常：${err}`);
+}
