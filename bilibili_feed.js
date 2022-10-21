3ab951fac0c99d97897bd921cca9cae20c0146c5
yuanbp <yuanbp0912@163.com>
2022-10-21 15:39:23 +0800

    Update bilibili_feed.js

--- bilibili_feed.js
+++ bilibili_feed.js
@@ -1,13 +1,13 @@
 let res = $response.body;
 let body = JSON.parse(res);
 
-let items=body['data']['items'].filter(e => {
+let items=body.data.items.filter(e => {
   //if(e.hasOwnProperty('banner_item')){
-  //  delete e['banner_item'];
+  //  delete e.banner_item;
   //}
-  return ((e.hasOwnProperty('card_goto') && !e['card_goto'].startsWith('ad_')) && !e.hasOwnProperty('ad_info'));
+  return ((e.hasOwnProperty('card_goto') && !e.card_goto.startsWith('ad_')) && !e.hasOwnProperty('ad_info'));
 })
-body['data']['items']=items;
+body.data.items = items;
 
 let result = JSON.stringify(body);
 
