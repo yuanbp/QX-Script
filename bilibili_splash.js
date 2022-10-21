4356d34a834d6b830dd325725863b50985bc8291
yuanbp <yuanbp0912@163.com>
2022-10-21 17:55:46 +0800

    Update bilibili_splash.js

--- bilibili_splash.js
+++ bilibili_splash.js
@@ -1,7 +1,7 @@
 try {
   //delete $response.body;
   //console.log(JSON.stringify($response));
-  console.log(JSON.stringify($request.path));
+  console.log($request.path);
   
   let res = $response.body;
   let body = JSON.parse(res);
