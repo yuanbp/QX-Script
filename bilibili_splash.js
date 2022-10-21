e4a2074442289f01dcfe1ca51477088bc56e7eff
yuanbp <yuanbp0912@163.com>
2022-10-21 17:37:18 +0800

    Update bilibili_splash.js

--- bilibili_splash.js
+++ bilibili_splash.js
@@ -1,21 +1,22 @@
 //delete $response.body;
 //console.log(JSON.stringify($response));
-console.log(JSON.stringify($request));
+//console.log(JSON.stringify($request));
 
 let res = $response.body;
 let body = JSON.parse(res);
 
-//body.data = {};
-body.data.max_time = 0;
-body.data.min_interval = 31536000;
-body.data.pull_interval = 31536000;
-for (let i = 0; i < body.data.list.length; i++) {
-  body.data.list[i].duration = 0;
-  body.data.list[i].begin_time = 1915027200;
-  body.data.list[i].end_time = 1924272000;
+if($request.path.startsWith('/x/v2/splash/list?'))) {
+  body.data.max_time = 0;
+  body.data.min_interval = 31536000;
+  body.data.pull_interval = 31536000;
+  for (let i = 0; i < body.data.list.length; i++) {
+    body.data.list[i].duration = 0;
+    body.data.list[i].begin_time = 1915027200;
+    body.data.list[i].end_time = 1924272000;
+  }
+} else {  
+  body.data = {};
 }
 result = JSON.stringify(body);
 
 //console.log(result);
-
-$done(result);
