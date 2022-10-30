7b1e2ec2795883c4468af59b31178cdae4a227c2
yuanbp <yuanbp0912@163.com>
2022-10-30 10:13:59 +0800

    Create pdf_expert.js

---
+++ scripts/pdf_expert.js
@@ -0,0 +1,17 @@
+try{
+  let res = $response.body;
+  let body = JSON.parse(res);
+
+  let body = {};
+  let jsonStr= '{"originalTransactionId":"730000355073823","subscriptionState":"active","isInGracePeriod":true,"subscriptionExpirationDate":"02:33 19\/08\/2020","subscriptionAutoRenewStatus":"autoRenewOff","isEligibleForIntroPeriod":true,"isPDFExpert6User":true,"subscriptionReceiptId":"1559207582000"}';
+  body = JSON.parse(jsonStr);
+  body.subscriptionExpirationDate = '10:05 30\/10\/2023';
+  
+  let result = JSON.stringify(body);
+
+  //console.log(result);
+
+  $done(result);
+} catch (err) {
+  console.log(`pdf_expert.js 出现异常：${err}`);
+}
