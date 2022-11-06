21962ea123e6c2e347d89b68135e2d3724f7bfcc
yuanbp <yuanbp0912@163.com>
2022-11-06 16:44:48 +0800

    Update pdf_expert2.js

--- scripts/pdf_expert2.js
+++ scripts/pdf_expert2.js
@@ -3,12 +3,12 @@ try{
   //console.log(res);
   let body = JSON.parse(res);
 
-  delete body.inAppStates[0].subscriptionExpirationIntent;
-  body.inAppStates[0].subscriptionExpirationDate = '12:04 01/11/2023';
-  body.inAppStates[0].subscriptionState = 'active';
-  body.inAppStates[0].isEligibleForIntroPeriod = true;
-  body.inAppStates[0].isInGracePeriod = true;
-  body.inAppStates[0].isPDFExpert6User = true;
+  //delete body.inAppStates[0].subscriptionExpirationIntent;
+  //body.inAppStates[0].subscriptionExpirationDate = '12:04 01/11/2023';
+  //body.inAppStates[0].subscriptionState = 'active';
+  //body.inAppStates[0].isEligibleForIntroPeriod = true;
+  //body.inAppStates[0].isInGracePeriod = true;
+  //body.inAppStates[0].isPDFExpert6User = true;
 
   let result = JSON.stringify(body);
 
